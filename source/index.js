5d2dbf5d652c2d617736479c8228de8ad3ae505d
Petterson Alves <pettalves@gmail.com>
2018-03-23 10:55:03 -0300

    Create carousel

--- source/index.js
+++ source/index.js
@@ -10,6 +10,7 @@ const sectionLinks = document.querySelector('.section-links');
 const sectionActivities = document.querySelector('.section-activities');
 const closeModals = document.querySelectorAll('.close-modal');
 const noScroll = document.querySelector('body');
+const carouselOptions = document.querySelectorAll('.section-carousel .option');
 
 greenButtons.forEach(function(greenButton) {
   greenButton.onclick = function() {
@@ -38,5 +39,42 @@ closeModals.forEach(function(closeModal) {
     sectionLinks.classList.remove('-active');
     sectionActivities.classList.remove('-active');
     noScroll.classList.remove('-noscroll');
+  }
+})
+
+// carousel
+function startCarousel() {
+  const activeItem = document.querySelector('.section-carousel .item.-active');
+  const activeOption = document.querySelector('.section-carousel .option.-active');
+  if (activeItem.nextElementSibling) {
+    activeItem.nextElementSibling.classList.add('-active');
+    activeOption.nextElementSibling.classList.add('-active');
+  } else {
+    activeItem.parentNode.firstElementChild.classList.add('-active');
+    activeOption.parentNode.firstElementChild.classList.add('-active');
+  }
+  activeItem.classList.remove('-active');
+  activeOption.classList.remove('-active');
+}
+
+const time = 5000;
+document.querySelector('.section-carousel .item:first-child').classList.add('-active');
+let carousel = setInterval(startCarousel, time);
+
+carouselOptions.forEach(function(carouselOption) {
+  carouselOption.onclick = function(e) {
+    const itemIndex = e.target.dataset.item;
+
+    const activeItem = document.querySelector('.section-carousel .item.-active');
+    const activeOption = document.querySelector('.section-carousel .option.-active');
+    activeItem.classList.remove('-active');
+    activeOption.classList.remove('-active');
+
+    const item = document.querySelectorAll('.section-carousel .item')[itemIndex];
+    item.classList.add('-active');
+    e.target.classList.add('-active');
+
+    clearInterval(carousel);
+    carousel = setInterval(startCarousel, time);
   }
 })
