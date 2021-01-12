205357b5c501b37a3c0dc37b1a922c70c72a38b8
erivanio <erivanio.vanin@gmail.com>
2021-01-12 14:46:51 -0300

    add lingumania js and JSON translation data

---
+++ source/translate/JSON-translation-data.js
@@ -0,0 +1,32 @@
+﻿var linguJSON = {
+    "languages": [
+        {
+            "lang_name": "Português",
+            "lang_code": "pt",
+            "url_pattern": "?"
+        },
+        {
+            "lang_name": "English",
+            "lang_code": "en",
+            "url_pattern": "?"
+        },
+    ],
+    "translated_segments": [
+        {
+            "source": "Auxiliamos na implementação de um parlamento aberto",
+            "target": "We assist in the implementation of an open parliament"
+        },
+        {
+            "source": "Promovemos ações de transparência legislativa",
+            "target": "We promote legislative transparency actions"
+        },
+        {
+            "source": "Desenvolvemos projetos de participação popular",
+            "target": "We develop popular participation projects"
+        },
+        {
+            "source": "Nossas atividades",
+            "target": "Our activities"
+        },
+    ],
+};
